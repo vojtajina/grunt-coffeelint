ad19944c2d715d1d96dc39b5cac6c7a40d79810a
Ed Siok <sioked@gmail.com>
2012-10-10 18:23:23 -0500

    Updated to only display errors or warnings, and to fail when any warnings occur

--- tasks/coffeelint.js
+++ tasks/coffeelint.js
@@ -5,18 +5,30 @@ module.exports = function(grunt) {
 
     var files = grunt.file.expandFiles(this.data.files || this.data);
     var options = this.data.options || grunt.config('coffeelintOptions') || {};
+    var errorCount = 0;
 
     files.forEach(function(file) {
-      grunt.log.writeln('Linting ' + file + '...');
+      grunt.verbose.writeln('Linting ' + file + '...');
       var errors = coffeelint.lint(grunt.file.read(file), options);
-
+      
       if (!errors.length) {
-        grunt.log.ok('Lint free.')
+        grunt.verbose.ok();
       } else {
         errors.forEach(function(error) {
-          grunt.log.error(file + ':' + error.lineNumber + ' ' + error.message + ' (' + error.rule + ')');
+          var status = "[warn]".yellow;
+          if(/error/.test(error.level)){
+            errorCount += 1;
+            status = "[error]".red;
+          }
+          grunt.log.writeln(status + ' ' +file + ':' + error.lineNumber + ' ' + error.message + ' (' + error.rule + ')');
         });
       }
     });
+    
+    if(!errorCount){
+      grunt.log.write("Lint free");
+    } else {
+      return false;
+    }
   });
 };
